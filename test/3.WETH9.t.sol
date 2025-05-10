685b025e9f5c49577d9cf008df9fdd73b2b39bbb
0xcharlie <charlyfinos@gmail.com>
2025-05-10 16:37:59 +0200

    add inv and helper

--- test/3.WETH9.t.sol
+++ test/3.WETH9.t.sol
@@ -37,23 +37,36 @@ contract WETH9Test is Test {
         assertEq(address(weth).balance, weth.totalSupply());
     }
 
+    /// @dev Helper function to calculate sum of all WETH balances across actors
+    function _sumOfBalances() internal view returns (uint256) {
+        uint256 sumOfAllBalances;
+        address[] memory actors = handler.actors();
+
+        for (uint256 i; i < actors.length; i++) {
+            sumOfAllBalances += weth.balanceOf(actors[i]);
+        }
+
+        return sumOfAllBalances;
+    }
+
     /// INVARIANT 4) The ETH balance on WETH should be equal to the sum of all deposits
     /// For implementing this one we need to track balances of all users
     /// Once we have the actor arrays we can sum up all the balances of all users and compare it with the WETH balance
     /// NOTE it may take longer since it has to loop through all actors
     function invariant_ETHbalanceEqToSumOfDeposits() public view {
-        //// declare sum of balances variable
-        uint256 sumOfAllBalances;
-
-        /// Save on memory the actors array
-        address[] memory actors = handler.actors();
-
-        /// Loop through all actors and sum up their weth balances
-        for (uint256 i; i < actors.length; i++) {
-            sumOfAllBalances += weth.balanceOf(actors[i]);
-        }
-
+        uint256 sumOfAllBalances = _sumOfBalances();
         assertEq(address(weth).balance, sumOfAllBalances);
+    }
+
+    /// INVARIANT 5) sum of all balances should not exceed total supply
+    function invariant_sumOfBalancesShouldNotExceedSupply() public view {
+        uint256 sumOfAllBalances = _sumOfBalances();
+        assertLe(sumOfAllBalances, weth.totalSupply());
+    }
+
+    /// INVARIANT 6) user balance not higher than the total supply
+    function invariant_userBalanceNotHigherThanTotalSupply() public view {
+        assertLe(weth.balanceOf(msg.sender), weth.totalSupply());
     }
 
     /// Adding this function made the test SolvencyDeposits() pass
